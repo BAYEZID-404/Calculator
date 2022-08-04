93a6276bcda6f67d3e603c1e8aa7a53e8e554beb
Minhaz Hossain <107618111+Minhaz-AK313@users.noreply.github.com>
2022-08-04 14:36:52 +0600

    Create Termux-Calculator.py
    git clone https://github.com/Minhaz-AK313/Calculator
    cd Calculator
    ls
    python Termux-Calculator.py

---
+++ Termux-Calculator.py
@@ -0,0 +1,306 @@
+#            🔥Minhaz-Ak313🔥
+
+from time import sleep
+
+import os
+
+B="\033[1;34m"        # Blue
+
+O="\033[0m"       # Text Reset
+
+P="\033[1;35m"      # Purple
+
+R="\033[1;31m"         # Red
+
+G="\033[1;32m"       # Green
+
+Y="\033[1;33m"      # Yellow
+
+C="\033[1;36m"        # Cyan  
+
+logo = ("""\033[1;36m
+
+███╗   ███╗██╗███╗   ██╗██╗  ██╗ █████╗ ███████╗
+
+████╗ ████║██║████╗  ██║██║  ██║██╔══██╗╚══███╔╝
+
+██╔████╔██║██║██╔██╗ ██║███████║███████║  ███╔╝ 
+
+██║╚██╔╝██║██║██║╚██╗██║██╔══██║██╔══██║ ███╔╝  
+
+██║ ╚═╝ ██║██║██║ ╚████║██║  ██║██║  ██║███████╗
+
+╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝                              
+
+\033[1;35m════════════════════════════════════════════════════════════
+
+\033[1;92mFACEBOOK      : Minhaz Hossain 
+
+GITHUB        : Minhaz-Ak313
+
+FACEBOOK PAGE : Minhaz-Ak313
+
+CODER         : Minhaz Hossain 
+
+             \033[1;31mWELCOM TO MINHAZ-Ak313 TOOLS
+
+\033[1;35m════════════════════════════════════════════════════════════
+
+""")
+
+print(logo)
+
+print("""\033[1;33m
+
+[ 1 ] Mini Calculator
+
+[ 2 ] Advance Calculator
+
+[ 3 ] NASA HACK (JUST FOR FUN)
+
+[ 4 ] FACEBOOK ACCOUNT 
+
+[ 5 ] FACEBOOK PAGE
+
+[ 6 ] GITHUB 
+
+[ 7 ] CLAY HACKER GROUP 
+
+[ 8 ] CLAY HACKER YOTUBE CHANNEL (\033[1;32mClay Hacker Yt\033[1;33m)
+
+[ 9 ] GITHUB (\033[1;32mBD-CLAY-Hacker\033[1;33m)
+
+                    🔥 \033[1;36mMinhaz-Ak313🔥   """)
+
+print(" ")
+
+minhaz = input("\033[1;36mSelect a option \033[1;31m==> \033[0m")
+
+if minhaz == "1" :
+
+    os.system("clear")
+
+    print("calculator is starting.... ")
+
+    sleep(2)
+
+    print(" ")
+
+    first = input("\033[1;32mFirst number \033[1;36m~==> ")
+
+    print("                \033[1;36m{ +    -    /    *  }")
+
+    print(" ")
+
+    print("""
+
+    \033[1;34m+   Addition     
+
+    \033[1;35m-   Substraction 
+
+    \033[1;31m*   Multiplication 
+
+    \033[1;32m/   Division 
+
+    """)
+
+    operator = input("\033[1;33mOperator { +, -, *, / } \033[1;36m~==> ")
+
+    second = input("\033[1;32mSecond number  \033[1;36m~==> ")
+
+    first = int(first)
+
+    second = int(second)
+
+    if operator == "+" :
+
+        print(first+second)
+
+    elif operator == "-" :
+
+        print(first-second)
+
+    elif operator == "*" :
+
+        print(first*second)
+
+    elif operator == "/" :
+
+        print(first/second)
+
+    else:
+
+        print("\033[1;31minvalid operation")
+
+elif minhaz == "2":
+
+    print(" Calculator is starting....")
+
+    sleep(2)
+
+   
+
+    
+
+    print(" ")
+
+    print("                \033[1;36m{ +    -    /    *  }")
+
+    print(" ")
+
+    print("""
+
+    \033[1;34m+   Addition     
+
+    \033[1;35m-   Substraction 
+
+    \033[1;31m*   Multiplication 
+
+    \033[1;32m/   Division 
+
+    """)
+
+    print(eval(input("\033[1;35mEnter number and math operation \033[1;31m==>\033[1;36m ")))     
+
+elif minhaz == "3":
+
+    os.system("clear")
+
+    R = input(" Enter Your Anonymous Name : ")
+
+    os.system("clear")
+
+    print("\033[1;32mTergat Is LOCK.....")
+
+    sleep(2)
+
+    print("Collecting Data.....")
+
+    sleep(1)
+
+    print("Crack Password.......")
+
+    sleep(1.5)
+
+    print("Sacnning System........")
+
+    sleep(2)
+
+    print("System Loading...........")
+
+    sleep(2)
+
+    print("Crack System...............")
+
+    sleep(2)
+
+    for x in range(1,100):
+
+        sleep(0.08)
+
+        print(R+"🔥>>>Nasa Hacking Loading......"+str(x)+"%")
+
+        
+
+    print(R+"🔥>>>System crack successful.....100%")
+
+    print("\033[1;31m════════════════════════════════════════════════════════════")
+
+    print(" ")
+
+    print("                 \033[1;31mHACKED BY "+R+"🔥")     
+
+    print(" ")
+
+    print("════════════════════════════════════════════════════════════")
+
+elif minhaz == "4":
+
+    os.system("clear")
+
+    print(" ")
+
+    print("""\033[1;35m
+
+    Name- \033[1;32m Minhaz Hossain 
+
+    
+
+    
+
+   \033[1;35m FB ID LINK- \033[1;32mhttps://www.facebook.com/profile.php?id=100077643847748""")  
+
+   
+
+elif minhaz == "5":
+
+    os.system(" clear")
+
+    print("""\033[1;35m
+
+    Name- \033[1;32m Minhaz-Ak313 
+
+    
+
+    
+
+   \033[1;35m FB PAGE LINK- \033[1;32mhttps://www.facebook.com/profile.php?id=100083163184657""") 
+
+   
+
+elif minhaz == "6" :
+
+    os.system("clear")
+
+    print("""\033[1;35m
+
+    Name- \033[1;32m Minhaz Hossain
+
+    
+
+    \033[1;33mGITHUB LINK - \033[1;32mhttps://www.github.com/Minhaz-Ak313""")
+
+elif minhaz == "7":
+
+    os.system(" clear")
+
+    print("""
+
+    \033[1;35mName- \033[1;32mClay Hacker
+
+    
+
+    \033[1;33mGroup Link- \033[1;32m https://facebook.com/groups/clayhackers2.0/
+
+    
+
+    """)
+
+elif minhaz == "8":
+
+    print("""
+
+    \033[1;35mName-\033[1;32m Clay Hacker Yt
+
+    
+
+    \033[1;33mChannel Link-\033[1;32m https://youtube.com/channel/UCw8yIBh24F7enUiiEDdEUyQ
+
+    
+
+    
+
+    """)
+
+elif minhaz == "9":
+
+    print("""
+
+    \033[1;35mName-\033[1;32m BD-CLAY-HACKER
+
+    
+
+    \033[1;33mGITHUB LINK- \033[1;32mhttps://www.github.com/BD-CLAY-Hacker
+
+    """)
+
